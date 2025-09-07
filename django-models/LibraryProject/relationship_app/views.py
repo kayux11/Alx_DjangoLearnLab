2065df81ba6a7310bc6bc1f22b24d2be7d74a993
Lameck1 <lameck72@gmail.com>
2025-09-08 00:23:26 +0300

    Solve persmision required bug

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.views.generic.detail import DetailView
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect
 from .models import Book, Library
 from .forms import BookForm
