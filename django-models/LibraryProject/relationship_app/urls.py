92d7873269d9253a54a2571b6c6479b35fc561f2
Lameck1 <lameck72@gmail.com>
2025-09-08 00:04:10 +0300

    Use builtin auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    register_view,
-    login_view,
-    logout_view
-)
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views  # To access the register view
 
 urlpatterns = [
-    path('', list_books, name='home'),
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    
+    path('', views.list_books, name='home'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
     # Authentication URLs
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),  # must reference views.register
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
