90d05b314ae1743ccf2cc5f9b820b38dabf8b63c
Lameck1 <lameck72@gmail.com>
2025-09-07 23:50:22 +0300

    Fix Class-based view bug

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # <-- Make sure Library is imported
 
 # --- Function-Based View ---
 def list_books(request):
     books = Book.objects.all()
-    # Correct template path with app namespace
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # --- Class-Based View ---
 class LibraryDetailView(DetailView):
     model = Library
-    # Correct template path with app namespace
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # Correct template path
     context_object_name = 'library'
