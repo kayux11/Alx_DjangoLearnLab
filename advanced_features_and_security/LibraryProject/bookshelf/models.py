ad01f5deba6f6e041c2e669b5d2a11c0508ebd43
abdulkarim sharafudeen adegboye <abdulkarimsarafadeen@gmail.com>
2025-09-12 17:15:13 +0100

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-    isbn = models.CharField(max_length=13, unique=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return self.title
